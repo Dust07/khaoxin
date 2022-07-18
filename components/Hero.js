f63f793bff42de5efb3fbed32466a6a1c2e60c14
Dust07 <tienphat0707@gmail.com>
2022-07-18 21:14:29 +0700

    Init project

---
+++ components/Hero.js
@@ -0,0 +1,12 @@
+
+function Hero() {
+  return (
+    <section className="section-hero">
+      <div className="container">
+        <p>Hero Page</p>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
---
+++ pages/about-us/index.js
@@ -0,0 +1,14 @@
+import Header from "../../components/Header"
+import Hero from "../../components/Hero"
+
+function AboutUs() {
+  return (
+    <>
+      <Header></Header>
+      <Hero></Hero>
+    </>
+
+  )
+}
+
+export default AboutUs
