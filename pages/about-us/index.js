c38bbdccc3a99dfd5e6ff85d35460218b805bdd1
Dust07 <tienphat0707@gmail.com>
2022-08-15 12:25:30 +0700

    refactor code

--- pages/about-us/index.js
+++ pages/about-us/index.js
@@ -1,5 +1,5 @@
-import Header from "../../components/Header"
-import Hero from "../../components/Hero"
+import Header from "../../components/layout/header/Header"
+import Hero from "../../components/hero/Hero"
 
 function AboutUs() {
   return (
