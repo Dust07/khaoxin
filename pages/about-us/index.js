12af4cdbb3cf9f7cd53043d9cfeb2eea264c69d5
Dust07 <tienphat0707@gmail.com>
2022-09-17 14:37:21 +0700

    Added about us page

--- pages/about-us/index.js
+++ pages/about-us/index.js
@@ -1,12 +1,13 @@
 import Hero from "../../components/hero/Hero"
 import Fade from "../../components/animated_component/Fade"
+import AboutUsContent from "../../components/about_us/AboutUsContent"
 
 function AboutUs() {
   return (
     <Fade>
       <Hero />
+      <AboutUsContent />
     </Fade>
-
   )
 }
 
