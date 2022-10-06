89f487cf17344cbf7cf33c566eb50393a2ff8ac6
phatltt <phatltt@rikkeisoft.com>
2022-10-06 10:28:20 +0700

    Test geolocation

--- components/reusable/btn/GetDirectionBtn.js
+++ components/reusable/btn/GetDirectionBtn.js
@@ -2,7 +2,7 @@ import btnStyles from "./Button.module.scss"
 
 function GetDirectionBtn({ text, getDirectionURL }) {
   const handleClick = (url) => {
-    window.location(url);
+    window.location.href(url);
   }
   return (
     <button className={`${btnStyles.getDirectionBtn}`} onClick={() => handleClick(getDirectionURL)}>
