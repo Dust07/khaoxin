09fa47b2448782715bc4106060f6050e2aa669c0
phatltt <phatltt@rikkeisoft.com>
2022-10-06 10:39:12 +0700

    Test geolocation

--- components/reusable/btn/GetDirectionBtn.js
+++ components/reusable/btn/GetDirectionBtn.js
@@ -1,8 +1,13 @@
 import btnStyles from "./Button.module.scss"
+import { isMobile } from "react-device-detect";
 
 function GetDirectionBtn({ text, getDirectionURL }) {
   const handleClick = (url) => {
-    window.location.href(url);
+    if (isMobile) {
+      window.location.assign(url);
+    } else {
+      window.open(url);
+    }
   }
   return (
     <button className={`${btnStyles.getDirectionBtn}`} onClick={() => handleClick(getDirectionURL)}>
