067da6cf50b062a573431ae162346e031e0c24cd
Dust07 <tienphat0707@gmail.com>
2022-11-19 16:33:50 +0700

    Test GeoLocation on localhost

--- components/location_detail/LocationDetail.js
+++ components/location_detail/LocationDetail.js
@@ -1,14 +1,44 @@
 import locationStyles from "./LocationDetail.module.scss"
-import { location } from "../../database/location.js"
+// import { location } from "../../database/location.js"
 import LocationItem from "./location_item/LocationItem"
+import { useEffect, useState } from "react"
 
 function LocationDetail() {
+  const [location, setLocation] = useState({ lat: null, lng: null })
+  const [errorMessage, setErrorMessage] = useState("")
+  useEffect(() => {
+    console.log(location)
+  }, [location])
+
+  const updatePosition = (position) => {
+    console.log(position)
+    setLocation({
+      lat: position.coords.latitude,
+      lng: position.coords.longitude,
+    })
+  }
+
+  const error = () => {
+    console.log("GPS ERROR")
+    setErrorMessage("GPS DISABLED, PLEASE ENABLE")
+  }
+
+  const option = {
+    maximumAge: 0
+  }
+
+  const getGeoLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(updatePosition, error, option)
+    }
+  }
+
   return (
     <section className={locationStyles.locationDetailWrapper}>
       <h1 className={locationStyles.h1Text}>Danh sách cửa hàng Khaoxin</h1>
       <div className={`container ${locationStyles.locationContainer}`}>
         <div className={locationStyles.locationListContainer}>
-          <ul className={locationStyles.locationList}>
+          {/* <ul className={locationStyles.locationList}>
             {location.map(item => {
               return (
                 <LocationItem key={item.id} location={item} selected={true} />
@@ -20,9 +50,14 @@ function LocationDetail() {
             <li className={locationStyles.listItemComingSoon}>
               <label htmlFor={location.id}>COMING SOON</label>
             </li>
-          </ul>
+          </ul> */}
         </div>
       </div>
+
+      {errorMessage && <p>{errorMessage}</p>}
+      <button onClick={getGeoLocation}>GET CURRENT POSITION</button>
+      <p>Lat:{location.lat}</p>
+      <p>Lng:{location.lng}</p>
     </section>
   )
 }
